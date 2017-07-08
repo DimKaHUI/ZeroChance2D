bb909b1872dd95840cdce602864c3b6d730c01c2
Dmitry <dimamon6032403@gmail.com>
2017-07-08 21:31:22 +0300

    Inital version of IHuman created

---
+++ Assets/Scripts/IHuman.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZeroChance2D
+{
+    public interface IHuman
+    {
+        string Forename { get; set; }
+        string Surname { get; set; }
+
+        float WalkSpeed { get; set; }
+        float RunSpeed { get; set; }
+    }
+
+}
