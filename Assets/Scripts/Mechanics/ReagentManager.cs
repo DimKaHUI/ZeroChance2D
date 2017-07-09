8b5526e1527993b965afbd3be77cf6a2513678fb
Dmitry <dimamon6032403@gmail.com>
2017-07-09 20:46:56 +0300

    Created ReagentManager class
    This class will store all possible chemical reactions and be responsible for chemistry in the game.

---
+++ Assets/Scripts/Mechanics/ReagentManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Reagent
+{
+    private float amount;
+    public readonly string Name;
+
+    public float Amount
+    {
+        get { return amount; }
+        set
+        {
+            if (value >= 0)
+                amount = value;
+            else
+                amount = 0;
+        }
+    };
+
+    public Reagent(string name, float amount)
+    {
+        Name = name;
+        this.amount = amount;
+    }
+}
+
+public class ReagentManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
