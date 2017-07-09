d07c18a87d4cbdfa0a0a70eab6777b53b0850b9c
Dmitry <dimamon6032403@gmail.com>
2017-07-09 15:48:17 +0300

    Storage inteface created

---
+++ Assets/Scripts/IStorage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZeroChance2D
+{
+
+    public interface ICrate
+    {
+
+        int SlotVolume { get; }
+        List<IItem> StoredItems { get; set; }
+        bool AddItem(IItem item);
+        bool RemoveItem(IItem item);
+    }
+
+}
