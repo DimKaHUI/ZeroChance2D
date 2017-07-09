ce68d0d49539672b2d513070af83a9a5e91f8dd5
Dmitry <dimamon6032403@gmail.com>
2017-07-09 15:48:07 +0300

    Reagent container interface created

---
+++ Assets/Scripts/IReagentContainer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZeroChance2D
+{
+    public interface IReagentContainer
+    {
+        float Volume { get; }
+        float Amount { get; }
+        float TransferReagents(IReagentContainer target, float amount);
+        List<IReagent> AddReagents(IReagent[] reagents);
+        float FlushReagents();
+    }
+}
