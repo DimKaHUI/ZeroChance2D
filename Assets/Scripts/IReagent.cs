5e63d7834a8324346f34ca57557ff279145a1be9
Dmitry <dimamon6032403@gmail.com>
2017-07-09 15:47:53 +0300

    Reagent interface created

---
+++ Assets/Scripts/IReagent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZeroChance2D
+{
+
+    public interface IReagent
+    {
+        float Amount { get; set; }
+        string Description { get; }
+    }
+}
