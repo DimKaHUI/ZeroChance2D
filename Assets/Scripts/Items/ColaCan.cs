b0370896fbec272878fcb2e57d2be191aa023398
Dmitry <dimamon6032403@gmail.com>
2017-07-09 21:12:39 +0300

    ColaCan script created

---
+++ Assets/Scripts/Items/ColaCan.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZeroChance2D
+{
+
+    public class ColaCan : MonoBehaviour, IReagentContainer
+    {
+        public bool InitiallyFull;
+        public float InitialVolume;
+
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+
+    }
+}
