62e396a24fba1b116156b152a0324458bd35b745
Dmitry <dimamon6032403@gmail.com>
2017-07-09 15:47:43 +0300

    Item interface created

---
+++ Assets/Scripts/IItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZeroChance2D
+{
+
+    public interface IItem
+    {
+        float Weight { get; set; }
+        int SlotSize { get; set; }
+    }
+}
