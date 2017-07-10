d046385f0f08207b8972169fb40f19cc53df8abd
Dmitry <dimamon6032403@gmail.com>
2017-07-10 13:37:56 +0300

    UIManager class created

---
+++ Assets/Scripts/Mechanics/UIManger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace ZeroChance2D
+{
+
+    public enum HandSide { Left, Right}
+    public class UIManger : NetworkBehaviour
+    {
+        public Image LeftHandImage;
+        public Image RightHandImage;
+        public Image HealthIndicator;
+        public HandSide ActiveHand;
+        public GameObject Player;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+
+        }
+    }
+}
