e7f0132fb87944f42acdf909b02a905bb40d4d02
Dmitry <dimamon6032403@gmail.com>
2017-07-10 15:03:00 +0300

    UIManager completed

--- Assets/Scripts/Mechanics/UIManger.cs
+++ Assets/Scripts/Mechanics/UIManger.cs
@@ -8,13 +8,17 @@ namespace ZeroChance2D
 {
 
     public enum HandSide { Left, Right}
+    public enum ControllMode { Interaction, Shooting}
     public class UIManger : NetworkBehaviour
     {
         public Image LeftHandImage;
         public Image RightHandImage;
         public Image HealthIndicator;
         public HandSide ActiveHand;
-        public GameObject Player;
+        public ControllMode CurrentMode;
+        private PlayerController attachedPlayerController;
+
+        private Rigidbody2D rig;
 
         // Use this for initialization
         void Start()
@@ -22,10 +26,35 @@ namespace ZeroChance2D
 
         }
 
-        // Update is called once per frame
+        
         void FixedUpdate()
         {
+            if (attachedPlayerController == null)
+            {
+                Debug.Log(attachedPlayerController);
+                return;
+            }
 
+            if (CurrentMode == ControllMode.Interaction)
+            {
+                float forward = Input.GetAxis("Vertical");
+                float turn = Input.GetAxis("Horizontal");
+                rig.velocity = rig.gameObject.transform.up * attachedPlayerController.WalkSpeed * forward;
+                rig.gameObject.transform.rotation = Quaternion.Euler(0, 0, rig.gameObject.transform.rotation.eulerAngles.z);
+                rig.angularVelocity = -turn * attachedPlayerController.RotationSpeed;
+            }
         }
+
+        public PlayerController AttachedController
+        {
+            get { return attachedPlayerController; }
+            set
+            {
+                attachedPlayerController = value;
+                rig = value.gameObject.GetComponent<Rigidbody2D>();
+            }
+        }
+
+    
     }
 }
