2d23a5c61a9c924a2de62e6f268cd18ba1e29c45
Dmitry <dimamon6032403@gmail.com>
2017-07-10 10:36:06 +0300

    Created Item component

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZeroChance2D
+{
+
+    public class Item : MonoBehaviour
+    {
+
+        public int SlotSize;
+        public float Weight;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
