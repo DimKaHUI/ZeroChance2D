0ed449320f9bf997c65775f2d3d90325652e607b
Dmitry <dimamon6032403@gmail.com>
2017-07-10 15:03:15 +0300

    Player controller created

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZeroChance2D
+{
+
+    public class PlayerController : Human
+    {
+        public float RotationSpeed = 60f;
+        private UIManger uiManger;
+
+        void Start()
+        {
+            uiManger = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManger>();
+            uiManger.AttachedController = this;
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
