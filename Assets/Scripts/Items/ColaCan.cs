431394f152ab8c4a4613c31d8a0bd951fb805e16
Dmitry <dimamon6032403@gmail.com>
2017-07-10 10:36:29 +0300

    Created cola can class

---
+++ Assets/Scripts/Items/ColaCan.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZeroChance2D
+{
+    public class ColaCan : Item
+    {
+
+        private float initWeight;
+        private ReagentContainer colaCan;
+
+        // Use this for initialization
+        void Start()
+        {
+            initWeight = Weight;
+            colaCan = gameObject.GetComponent<ReagentContainer>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Weight = initWeight + colaCan.Amount / 1000f;
+        }
+    }
+}
