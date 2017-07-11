14828d1194e0a8ef7e525dfcf68289b3788afffe
Dmitry <dimamon6032403@gmail.com>
2017-07-11 20:07:01 +0300

    Created Weapon class

---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZeroChance2D
+{
+    
+    public class Weapon : MonoBehaviour
+    {
+        public WeaponModels WeaponModel;
+        public enum WeaponShootingMode{Auto, SemiAuto}
+        public int ShootsPerMinute;
+        public WeaponShootingMode ShootingMode;
+        public Clip InsertedClip;
+
+        public float ScatterAngle;
+        /// <summary>
+        /// Bullet speed in meters per second
+        /// </summary>
+        public float InitialBulletSpeed;
+        
+    }
+}
