dad4d9bab2a5856141c1d4ef1aaae853716c4f3d
Dmitry <dimamon6032403@gmail.com>
2017-07-14 17:41:11 +0300

    Some functions from UIManager moved to PlayerController

--- Assets/Scripts/Mechanics/UIManger.cs
+++ Assets/Scripts/Mechanics/UIManger.cs
@@ -7,262 +7,46 @@ using UnityEngine.UI;
 
 namespace ZeroChance2D
 {
-    [Serializable]
-    public enum HandSide { Left, Right}
-    public enum ControllMode { Interaction, Shooting}
-    public class UIManger : NetworkBehaviour
+    public class UIManger : MonoBehaviour
     {
-        public NetworkManager NetManager;
         public Image LeftHandImage;
         public Image RightHandImage;
         public RawImage LeftItemImage;
         public RawImage RightItemImage;
         public Image HealthIndicator;
-        public HandSide ActiveHand;
-        public ControllMode CurrentMode;
-        private PlayerController Controller;
-        public Vector3 CameraRelativePosition;
-        public float CameraVelocityOffset;
-        public float AimSensitivity;
-        public float ItemPickRange = 30f;
 
-        private Rigidbody2D rig;
-        private GameObject cameraObject;
-        private Vector3 prevOffset;
+        private GameObject playerObj;
+        private Human playerHuman;
+        private PlayerController playerController;
 
-        private Item itemUnderCursor;
-        private GameObject movableUnderCursor;
-        private GameObject livingUnderCursor;
-
-        
-
-        // Use this for initialization
-        void Start()
+        public GameObject LocalPlayerObject
         {
-            cameraObject = GameObject.FindGameObjectWithTag("MainCamera");
-            if(cameraObject == null)
-                Debug.LogError("Camera not found!");
-            prevOffset = Vector3.zero;
-
-            if(NetManager == null)
-                Debug.LogError("No network manager attached!");
-        }
-
-
-        void FixedUpdate()
-        {
-#region Movement and camera controlling
-            Vector3 pos = Controller.gameObject.transform.position;
-            
-
-            if (Controller == null)
-            {
-                Debug.Log(Controller);
-                return;
-            }
-
-            if (Input.GetButtonDown("Change Mode"))
-            {
-                if (CurrentMode == ControllMode.Shooting)
-                {
-                    CurrentMode = ControllMode.Interaction;
-                    Cursor.visible = true;
-                    Cursor.lockState = CursorLockMode.None;
-                    prevOffset = Vector3.zero;
-                }
-                else
-                {
-                    CurrentMode = ControllMode.Shooting;
-                    Cursor.visible = false;
-                    Cursor.lockState = CursorLockMode.Locked;
-                }
-                Debug.Log("Mode changed");
-            }
-
-            if (CurrentMode == ControllMode.Interaction)
-            {
-                float forward = Input.GetAxis("Vertical");
-                float turn = Input.GetAxis("Horizontal");
-                rig.velocity = rig.gameObject.transform.up * Controller.WalkSpeed * forward;
-                rig.gameObject.transform.rotation =
-                    Quaternion.Euler(0, 0, rig.gameObject.transform.rotation.eulerAngles.z);
-                rig.angularVelocity = -turn * Controller.RotationSpeed;
-            }
-            if (CurrentMode == ControllMode.Shooting)
-            {
-                float forward = Input.GetAxis("Vertical");
-                float turn = Input.GetAxis("Horizontal");
-                rig.velocity = rig.gameObject.transform.up * Controller.WalkSpeed * forward;
-
-                Ray ray = cameraObject.GetComponent<Camera>().ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
-                var point = ray.GetPoint(CameraRelativePosition.z);
-                
-                float angle = Mathf.Atan2(point.y - pos.y, point.x - pos.x);
-                rig.gameObject.transform.rotation =
-                    Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg - 90);
-
-            }
-
-            if (CurrentMode == ControllMode.Interaction)
-                cameraObject.transform.position = pos +
-                                            CameraRelativePosition;
-            if (CurrentMode == ControllMode.Shooting)
-            {
-                float move_x = Input.GetAxis("Mouse X");
-                float move_y = Input.GetAxis("Mouse Y");
-                prevOffset += new Vector3(move_x, move_y, 0) * AimSensitivity;
-                cameraObject.transform.position = new Vector3(pos.x, pos.y, CameraRelativePosition.z) + prevOffset;
-            }
-#endregion
-
-#region Finding objects under cursor
-            var clickPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            clickPoint.z = Controller.gameObject.transform.position.z;
-
-            bool hitUI = RaycastWorldUI();
-
-            var hitItem = Physics2D.Raycast(clickPoint, new Vector2(0, 0), Mathf.Infinity, LayerMask.NameToLayer("Items"));
-            itemUnderCursor = hitItem.collider == null ? null : hitItem.collider.gameObject.GetComponent<Item>();
-
-            var hitMovable = Physics2D.Raycast(clickPoint, new Vector2(0, 0), Mathf.Infinity, LayerMask.NameToLayer("Movables"));
-            movableUnderCursor = hitMovable.collider == null ? null : hitItem.collider.gameObject;
-
-            var hitLiving = Physics2D.Raycast(clickPoint, new Vector2(0, 0), Mathf.Infinity, LayerMask.NameToLayer("Livings"));
-            livingUnderCursor = hitLiving.collider == null ? null : hitItem.collider.gameObject;
-
-            #endregion
-
-#region Item picking
-
-            if (CurrentMode == ControllMode.Interaction)
-            {
-                
-
-                if (Input.GetMouseButtonDown(0) && !hitUI)
-                {
-                    if (itemUnderCursor != null && Vector2.Distance(Controller.transform.position, itemUnderCursor.gameObject.transform.position) <= ItemPickRange) // If cursor is upon an item
-                    {
-                        switch (ActiveHand)
-                        {
-                            case HandSide.Right:
-                                if (Controller.Equipment.RightHandItem == null)
-                                {
-                                    itemUnderCursor.gameObject.SetActive(false);
-                                    Controller.Equipment.RightHandItem = itemUnderCursor;
-                                }
-                                break;
-                            case HandSide.Left:
-                                if (Controller.Equipment.LeftHandItem == null)
-                                {
-                                    itemUnderCursor.gameObject.SetActive(false);
-                                    Controller.Equipment.LeftHandItem = itemUnderCursor;
-                                }
-                                break;
-                            default:
-                                throw new ArgumentOutOfRangeException();
-                        }
-                    }
-                    else if( Vector2.Distance(Controller.transform.position, clickPoint) <= ItemPickRange)
-                    {
-                        switch (ActiveHand)
-                        {
-                            case HandSide.Left:
-                                if (Controller.Equipment.LeftHandItem != null)
-                                {
-                                    Controller.Equipment.LeftHandItem.gameObject.SetActive(true);
-                                    Controller.Equipment.LeftHandItem.gameObject.transform.position = clickPoint;
-                                    Controller.Equipment.LeftHandItem = null;
-                                }
-                                break;
-                            case HandSide.Right:
-                                if (Controller.Equipment.RightHandItem != null)
-                                {
-                                    Controller.Equipment.RightHandItem.gameObject.SetActive(true);
-                                    Controller.Equipment.RightHandItem.gameObject.transform.position = clickPoint;
-
-                                    Controller.Equipment.RightHandItem = null;
-                                }
-                                break;
-                            default:
-                                throw new ArgumentOutOfRangeException();
-                        }
-                    }
-                }
-
-            }
-            #endregion
-
-
-        }
-
-        void Update()
-        {
-            // Drawing sprites
-            if (Controller != null && Controller.Equipment != null)
-            {
-                if (Controller.Equipment.LeftHandItem != null)
-                {
-                   // TODO
-                    var image = Controller.Equipment.LeftHandItem.gameObject.GetComponent<SpriteRenderer>()
-                        .sprite.texture;
-                    var imageSize = new Vector2(image.width, image.height);
-
-                    if (imageSize.x > imageSize.y)
-                    {
-                        float coef = imageSize.y / imageSize.x;
-                        LeftItemImage.rectTransform.sizeDelta = new Vector2(55, 55 * coef);
-                    }
-                    else
-                    {
-                        float coef = imageSize.x / imageSize.y;
-                        LeftItemImage.rectTransform.sizeDelta = new Vector2(55 * coef, 55);
-                    }
-
-                    LeftItemImage.texture = image;
-                }
-                else
-                    LeftItemImage.texture = null;
-
-
-                if (Controller.Equipment.RightHandItem != null)
-                {
-                    var image = Controller.Equipment.RightHandItem.gameObject.GetComponent<SpriteRenderer>()
-                        .sprite.texture;
-                    var imageSize = new Vector2(image.width, image.height);
-
-                    if (imageSize.x > imageSize.y)
-                    {
-                        float coef = imageSize.y / imageSize.x;
-                        RightItemImage.rectTransform.sizeDelta = new Vector2(55, 55 * coef);
-                    }
-                    else
-                    {
-                        float coef = imageSize.x / imageSize.y;
-                        RightItemImage.rectTransform.sizeDelta = new Vector2(55 * coef, 55);
-                    }
-
-                    RightItemImage.texture = image;
-                   
-                }
-                else
-                    RightItemImage.texture = null;
-
-
-            }
-        }
-
-
-        public PlayerController AttachedController
-        {
-            get { return Controller; }
+            get { return playerObj; }
             set
             {
-                Controller = value;
-                rig = value.gameObject.GetComponent<Rigidbody2D>();
+                playerObj = value;
+                playerHuman = playerObj.GetComponent<Human>();
+                playerController = playerObj.GetComponent<PlayerController>();
             }
         }
 
-        bool RaycastWorldUI()
+        public Vector2 UnderCursorPoint
+        {
+            get
+            {
+                var clickPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                clickPoint.z = 0;
+                return clickPoint;
+            }
+        }
+        public GameObject UnderCursor(string layer)
+        {
+            var hitItem = Physics2D.Raycast(UnderCursorPoint, new Vector2(0, 0), Mathf.Infinity,
+                LayerMask.NameToLayer(layer));
+            return hitItem.collider == null ? null : hitItem.collider.gameObject;
+        }
+
+        public bool IsCursorUponUi()
         {
             PointerEventData pointerData = new PointerEventData(EventSystem.current);
 
@@ -282,13 +66,69 @@ namespace ZeroChance2D
             return false;
         }
 
-        public void SelectHand(bool RightSide)
+        void Update()
         {
-            if(RightSide)
-                ActiveHand = HandSide.Right;
+            if (playerHuman == null)
+            {
+                Debug.LogWarning("No player attached!");
+                return;
+            }
+            // Drawing sprites
+            if (playerHuman.Equipment.LeftHandItem != null)
+            {
+                var image = playerHuman.Equipment.LeftHandItem.GetComponent<SpriteRenderer>()
+                    .sprite.texture;
+                var imageSize = new Vector2(image.width, image.height);
+
+                if (imageSize.x > imageSize.y)
+                {
+                    float coef = imageSize.y / imageSize.x;
+                    LeftItemImage.rectTransform.sizeDelta = new Vector2(55, 55 * coef);
+                }
+                else
+                {
+                    float coef = imageSize.x / imageSize.y;
+                    LeftItemImage.rectTransform.sizeDelta = new Vector2(55 * coef, 55);
+                }
+
+                LeftItemImage.texture = image;
+            }
             else
             {
-                ActiveHand = HandSide.Left;
+                LeftItemImage.texture = null;
+            }
+
+
+            if (playerHuman.Equipment.RightHandItem != null)
+            {
+                var image = playerHuman.Equipment.RightHandItem.GetComponent<SpriteRenderer>()
+                    .sprite.texture;
+                var imageSize = new Vector2(image.width, image.height);
+
+                if (imageSize.x > imageSize.y)
+                {
+                    float coef = imageSize.y / imageSize.x;
+                    RightItemImage.rectTransform.sizeDelta = new Vector2(55, 55 * coef);
+                }
+                else
+                {
+                    float coef = imageSize.x / imageSize.y;
+                    RightItemImage.rectTransform.sizeDelta = new Vector2(55 * coef, 55);
+                }
+
+                RightItemImage.texture = image;
+
+            }
+            else
+                RightItemImage.texture = null;
+        }
+        public void SelectHand(bool RightSide)
+        {
+            if (RightSide)
+                playerController.ActiveHand = HandSide.Right;
+            else
+            {
+                playerController.ActiveHand = HandSide.Left;
             }
         }
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,31 +1,199 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 namespace ZeroChance2D
 {
-
-    public class PlayerController : Human
+    public enum HandSide
     {
-        public float RotationSpeed = 60f;
-        private UIManger uiManger;
+        Left,
+        Right
+    }
 
+    public enum ControllMode
+    {
+        Interaction,
+        Shooting
+    }
+
+    public class PlayerController : NetworkBehaviour
+    {
+        public HandSide ActiveHand;
+        public ControllMode CurrentMode;
+
+        public Vector3 CameraRelativePosition = new Vector3(0, 0, -10);
+        public float AimSensitivity = 1f;
+        public float ItemPickRange = 3f;
+
+        public GameObject ManipulatedItem;
+
+        private Rigidbody2D rig;
+        private GameObject cameraObject;
+        private Vector3 prevOffset = Vector3.zero;
+        private Human playerHuman;
+        private UIManger Ui;
+        
+
+        // Use this for initialization
         void Start()
         {
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, gameObject.transform.rotation.z);
+
             if (isLocalPlayer)
             {
-                uiManger = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManger>();
-                uiManger.AttachedController = this;
+                Ui = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManger>();
+                if(Ui == null)
+                    Debug.LogWarning("No UIManager found!");
+
+                Ui.LocalPlayerObject = gameObject;
+
+                cameraObject = GameObject.FindGameObjectWithTag("MainCamera");
+                if (cameraObject == null)
+                    Debug.LogError("Camera not found!");
+                prevOffset = Vector3.zero;
+                playerHuman = gameObject.GetComponent<Human>();
+                rig = gameObject.GetComponent<Rigidbody2D>();
             }
 
         }
-        // Update is called once per frame
-        void Update()
+
+        void FixedUpdate()
         {
 
+            if (!isLocalPlayer)
+                return;
+
+            #region Movement and camera controlling
+
+            Vector3 pos = gameObject.transform.position;
+
+
+            if (playerHuman == null)
+            {
+                Debug.LogWarning("PlayerController class not attached");
+                return;
+            }
+
+            if (Input.GetButtonDown("Change Mode"))
+            {
+                if (CurrentMode == ControllMode.Shooting)
+                {
+                    CurrentMode = ControllMode.Interaction;
+                    Cursor.visible = true;
+                    Cursor.lockState = CursorLockMode.None;
+                    prevOffset = Vector3.zero;
+                }
+                else
+                {
+                    CurrentMode = ControllMode.Shooting;
+                    Cursor.visible = false;
+                    Cursor.lockState = CursorLockMode.Locked;
+                }
+                Debug.Log("Mode changed");
+            }
+
+            if (CurrentMode == ControllMode.Interaction)
+            {
+                float forward = Input.GetAxis("Vertical");
+                float turn = Input.GetAxis("Horizontal");
+                rig.velocity = rig.gameObject.transform.up * playerHuman.WalkSpeed * forward;
+                rig.angularVelocity = -turn * playerHuman.RotationSpeed;
+            }
+            if (CurrentMode == ControllMode.Shooting)
+            {
+                float forward = Input.GetAxis("Vertical");
+                float turn = Input.GetAxis("Horizontal");
+                rig.velocity = rig.gameObject.transform.up * playerHuman.WalkSpeed * forward;
+
+                Ray ray = cameraObject.GetComponent<Camera>()
+                    .ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+                var point = ray.GetPoint(CameraRelativePosition.z);
+
+                float angle = Mathf.Atan2(point.y - pos.y, point.x - pos.x);
+                rig.gameObject.transform.rotation =
+                    Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg - 90);
+
+            }
+
+            if (CurrentMode == ControllMode.Interaction)
+                cameraObject.transform.position = pos + CameraRelativePosition;
+            if (CurrentMode == ControllMode.Shooting)
+            {
+                float move_x = Input.GetAxis("Mouse X");
+                float move_y = Input.GetAxis("Mouse Y");
+                prevOffset += new Vector3(move_x, move_y, 0) * AimSensitivity;
+                cameraObject.transform.position = new Vector3(pos.x, pos.y, CameraRelativePosition.z) + prevOffset;
+            }
+
+            #endregion
+
+            if(Ui != null)
+                ManipulatedItem = Ui.UnderCursor("Items");
+
+            if (Input.GetMouseButtonDown(0) && ManipulatedItem != null)
+            {
+                PickItem(ManipulatedItem);
+            }
+            if (Input.GetMouseButtonDown(0) && ManipulatedItem == null)
+            {
+                if (ActiveHand == HandSide.Left && playerHuman.Equipment.LeftHandItem != null)
+                    PutItem(ActiveHand);
+                if (ActiveHand == HandSide.Right && playerHuman.Equipment.RightHandItem != null)
+                    PutItem(ActiveHand);
+            }
+        }
+
+        void PickItem(GameObject item)
+        {
+            switch (ActiveHand)
+            {
+                case HandSide.Left:
+                    if (gameObject.GetComponent<Human>().Equipment.LeftHandItem == null)
+                    {
+                        item.GetComponent<Item>().Visible = false;
+                        gameObject.GetComponent<Human>().Equipment.LeftHandItem = item;
+                    }
+                    break;
+                case HandSide.Right:
+                    if (gameObject.GetComponent<Human>().Equipment.RightHandItem == null)
+                    {
+                        item.GetComponent<Item>().Visible = false;
+                        gameObject.GetComponent<Human>().Equipment.RightHandItem = item;
+                    }
+                    break;
+            }
+        }
+
+        void PutItem(HandSide side)
+        {
+            switch (side)
+            {
+                case HandSide.Left:
+                    CmdSetItemLocation(gameObject.GetComponent<Human>().Equipment.LeftHandItem, Ui.UnderCursorPoint);
+                    gameObject.GetComponent<Human>().Equipment.LeftHandItem = null;
+                    break;
+                case HandSide.Right:
+                    CmdSetItemLocation(gameObject.GetComponent<Human>().Equipment.RightHandItem, Ui.UnderCursorPoint);
+                    gameObject.GetComponent<Human>().Equipment.RightHandItem = null;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("side", side, null);
+            }
+        }
+
+        [Command]
+        void CmdSetItemLocation(GameObject item, Vector3 location)
+        {
+            item.transform.position = location;
+            item.GetComponent<Item>().Visible = true;
         }
 
         
+
     }
 }
+
