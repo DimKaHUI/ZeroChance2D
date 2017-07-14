8992eecd0e5ff15125c186025e6f094320be89dd
Dmitry <dimamon6032403@gmail.com>
2017-07-14 17:41:26 +0300

    Adde sync scripts

---
+++ Assets/Scripts/PlayerEquipmentSync.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace ZeroChance2D
+{
+
+    public class PlayerEquipmentSync : NetworkBehaviour
+    {
+        [SyncVar] public Equipment SyncEquipment;
+
+        void FixedUpdate()
+        {
+            if (!isLocalPlayer)
+            {
+                UpdateEquipment();
+            }
+            else
+            {
+                TransmitEquipment();
+            }
+        }
+
+
+        void UpdateEquipment()
+        {
+            gameObject.GetComponent<Human>().Equipment.LeftHandItem = SyncEquipment.LeftHandItem;
+            gameObject.GetComponent<Human>().Equipment.RightHandItem = SyncEquipment.RightHandItem;
+        }
+
+        [Client]
+        void TransmitEquipment()
+        {
+            CmdSendEquipmentToServer(gameObject.GetComponent<Human>().Equipment);
+        }
+
+        [Command]
+        void CmdSendEquipmentToServer(Equipment equipment)
+        {
+            SyncEquipment.LeftHandItem = equipment.LeftHandItem;
+            if(SyncEquipment.LeftHandItem != null)
+                SyncEquipment.LeftHandItem.GetComponent<Item>().Visible = false;
+            SyncEquipment.RightHandItem = equipment.RightHandItem;
+            if (SyncEquipment.RightHandItem != null)
+                SyncEquipment.RightHandItem.GetComponent<Item>().Visible = false;
+        }
+
+    }
+}
+
+
+/*
+ * string debugMsg = "Server data: Left: ";
+            if (SyncEquipment.LeftHandItem == null)
+                debugMsg += "none ";
+            else
+                debugMsg += SyncEquipment.LeftHandItem.GetComponent<Item>().ItemName;
+
+            debugMsg += " Right: ";
+            if (SyncEquipment.RightHandItem == null)
+                debugMsg += "none ";
+            else
+                debugMsg += SyncEquipment.RightHandItem.GetComponent<Item>().ItemName;
+
+            Debug.Log(debugMsg);
+*/
