eb1f7ee3dfd6febf6ddb8dd98c91a5abc8d19938
Dmitry <dimamon6032403@gmail.com>
2017-07-16 12:54:49 +0300

    Added previous manipulated item field

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -31,6 +31,7 @@ namespace ZeroChance2D
         public float ShootingModeMovementThresold = 0.5f;
 
         public GameObject ManipulatedItem;
+        public GameObject PrevManipulatedItem;
 
         private Rigidbody2D rig;
         private GameObject cameraObject;
