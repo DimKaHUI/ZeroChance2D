df35df1ffab8e54a1f8bc96d6228378a399516dc
Dmitry <dimamon6032403@gmail.com>
2017-07-18 21:15:04 +0300

    Created basic shooting

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace ZeroChance2D
 {
     
-    public class Weapon : MonoBehaviour
+    public class Weapon : NetworkBehaviour
     {
         public WeaponModels WeaponModel;
+        public GameObject BulletPrefab;
         public enum WeaponShootingMode{Auto, SemiAuto}
-        public int ShootsPerMinute;
+        public float CooldownDuration;
         public WeaponShootingMode ShootingMode;
         public AmmoClip InsertedClip;
         public AudioClip[] ShootAudioClips;
@@ -17,6 +19,27 @@ namespace ZeroChance2D
 
         public float ScatterAngle;
         public float InitialBulletSpeed;
+        public float BulletTimeout;
+
+        public bool ReadyToShoot = true;
+
+        public IEnumerator Cooldown()
+        {
+            yield return new WaitForSeconds(CooldownDuration);
+            ReadyToShoot = true;
+        }
+
+        [Command]
+        public void CmdUse(GameObject user)
+        {
+            var bullet = Instantiate(BulletPrefab, user.transform.position, user.transform.rotation);
+            bullet.GetComponent<Rigidbody2D>().velocity = user.transform.up * InitialBulletSpeed;
+            bullet.GetComponent<Bullet>().Lifetime = BulletTimeout;
+            NetworkServer.Spawn(bullet);
+            Debug.Log("Shoot!");
+            ReadyToShoot = false;
+            StartCoroutine(Cooldown());
+        }
         
     }
 }
