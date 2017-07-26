d84a83f06a12bc4e59dcc9809d7433d3a198258c
Dmitry <dimamon6032403@gmail.com>
2017-07-26 15:42:59 +0300

    Created StrorageSync script

---
+++ Assets/Scripts/Mechanics/StorageSync.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace ZeroChance2D
+{
+    [RequireComponent(typeof(Storage))]
+    public class StorageSync : NetworkBehaviour
+    {
+        [SyncVar]
+        public Inventory SyncInventory;
+
+        private Storage storage;
+
+        void Start()
+        {
+            storage = gameObject.GetComponent<Storage>();
+        }
+
+        void Update()
+        {
+            UpdateInventory();
+            ReceiveInventory();
+        }
+
+        [Server]
+        void UpdateInventory()
+        {
+            if (isServer)
+                SyncInventory = storage.Inventory;
+        }
+
+        [Client]
+        void ReceiveInventory()
+        {
+            if (!isServer)
+                storage.Inventory = SyncInventory;
+        }
+    }
+}
