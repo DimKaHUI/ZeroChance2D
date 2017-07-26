946e20c9c8e319953415d88f50cf9398e5548bd3
Dmitry <dimamon6032403@gmail.com>
2017-07-26 13:26:26 +0300

    Button script added

---
+++ Assets/Scripts/CExpButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZeroChance2D
+{
+
+    public class CExpButton : MonoBehaviour
+    {
+        public ZeroChance2D.CrateExplorer Explorer;
+        public GameObject ItemObj;
+        public void Click()
+        {
+            Explorer.ItemCallback(gameObject, ItemObj);
+        }
+    }
+}
