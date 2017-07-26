9277d1b4b3284fa1086cd1f398795d8d48e7add0
Dmitry <dimamon6032403@gmail.com>
2017-07-26 15:42:36 +0300

    Created ItemSync script

---
+++ Assets/Scripts/Mechanics/ItemSync.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace ZeroChance2D
+{
+    [RequireComponent(typeof(Item))]
+    public class ItemSync : NetworkBehaviour
+    {
+        [SyncVar]
+        public bool SyncVisible = true;
+
+        private Item item;
+
+        void Start()
+        {
+            item = gameObject.GetComponent<Item>();
+        }
+
+        void Update()
+        {
+            UpdateItemState();
+            ReceiveItemStatey();
+        }
+
+        [Server]
+        void UpdateItemState()
+        {
+            if (isServer)
+                SyncVisible = item.Visible;
+        }
+
+        [Client]
+        void ReceiveItemStatey()
+        {
+            if (!isServer)
+                item.Visible = SyncVisible;
+        }
+    }
+}
