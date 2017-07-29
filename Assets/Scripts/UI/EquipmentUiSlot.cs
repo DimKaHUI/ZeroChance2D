a9c60f45d4820ecd0576f5e3ce1ee5335b3fd416
Dmitry <dimamon6032403@gmail.com>
2017-07-29 22:28:10 +0300

    Created script for equipment slots

---
+++ Assets/Scripts/UI/EquipmentUiSlot.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZeroChance2D.Assets.Scripts.Mechanics;
+
+namespace ZeroChance2D.Assets.Scripts.UI
+{
+
+    public class EquipmentUiSlot : MonoBehaviour, IDropTarget, IDragTarget
+    {
+        public Equipment.EquipmentSlot Slot;
+
+        private UIManager uiManager;
+
+        public virtual bool ItemDragEnd(GameObject item)
+        {
+            if (uiManager == null)
+                return false;
+            if (uiManager.PlayerHuman.Equipment[Slot] == null)
+            {
+                uiManager.PlayerHuman.Equipment[Slot] = item;
+                return true;
+            }
+            return false;
+        }
+
+        public virtual GameObject DragGameObject
+        {
+            get { return uiManager.PlayerHuman.Equipment[Slot]; }
+        }
+
+        public virtual void OnSuccessfullDrag()
+        {
+            uiManager.PlayerHuman.Equipment[Slot] = null;
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            uiManager = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManager>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
