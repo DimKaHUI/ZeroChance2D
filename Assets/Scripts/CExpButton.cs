9f9aec6f869dac55dcd3e2acc2841d9c73ee2985
Dmitry <dimamon6032403@gmail.com>
2017-07-29 22:25:30 +0300

    Interface realization

--- Assets/Scripts/CExpButton.cs
+++ Assets/Scripts/CExpButton.cs
@@ -1,17 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using ZeroChance2D.Assets.Scripts.UI;
 
-namespace ZeroChance2D
+namespace ZeroChance2D.Assets.Scripts
 {
 
-    public class CExpButton : MonoBehaviour
+    public class CExpButton : MonoBehaviour, IDragTarget
     {
-        public ZeroChance2D.CrateExplorer Explorer;
+        public CrateExplorer Explorer;
         public GameObject ItemObj;
+
+        public GameObject DragGameObject
+        {
+            get { return ItemObj; }
+        }
+
+        public void OnSuccessfullDrag()
+        {
+            Explorer.DropEndCallback(gameObject, ItemObj);
+        }
+
         public void Click()
         {
             Explorer.ItemCallback(gameObject, ItemObj);
+        }
+
+        public void SwitchRaycast(bool isTarget)
+        {
+            //gameObject.transform.Find("Text").gameObject.GetComponent<Image>().raycastTarget = isTarget;
+            gameObject.GetComponent<Image>().raycastTarget = isTarget;
+            //gameObject.transform.Find("Image").gameObject.GetComponent<RawImage>().raycastTarget = isTarget;
         }
     }
 }
