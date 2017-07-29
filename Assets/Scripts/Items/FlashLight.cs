d41c61a6ec145f0d4b7b2e4f00b623022e2fc274
Dmitry <dimamon6032403@gmail.com>
2017-07-29 22:22:08 +0300

    Added new fields

--- Assets/Scripts/Items/FlashLight.cs
+++ Assets/Scripts/Items/FlashLight.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using ZeroChance2D.Assets.Scripts.Mechanics;
 
-namespace ZeroChance2D
+namespace ZeroChance2D.Assets.Scripts.Items
 {
     [RequireComponent(typeof(Collider2D))]
     [RequireComponent(typeof(NetworkTransform))]
@@ -13,8 +12,10 @@ namespace ZeroChance2D
     {
         [SyncVar]
         public bool TurnedOn = false;
+        // TODO DELETE UNUSED
         public float Range;
         public float VerticalOffset;
+        public float IntensityWhileKept;
 
         public float LerpRate = 30;
 
@@ -31,6 +32,9 @@ namespace ZeroChance2D
         public Vector3 lightSourcePos;
 
         [SyncVar] private float lightSourceZRotation;
+
+        public Texture defaultCookie;
+        public float defaultIntensity;
 
         public override void Use()
         {
@@ -73,15 +77,34 @@ namespace ZeroChance2D
         }
 
         [Server]
-        void Update()
+        void UpdateLight()
         {
             if (User == null)
             {
+                if (lightSource != null)
+                {
+                    lightSource.GetComponent<Light>().cookie = defaultCookie;
+                    lightSource.GetComponent<Light>().intensity = defaultIntensity;
+                }
                 lightSourcePos = gameObject.transform.position;
                 lightSourceZRotation = gameObject.transform.rotation.eulerAngles.z + 90;
             }
+            else if (User.GetComponent<Storage>() != null)
+            {
+                if (lightSource != null)
+                {
+                    lightSource.GetComponent<Light>().cookie = null;
+                    lightSource.GetComponent<Light>().intensity = IntensityWhileKept;
+                }
+            }
             else if (User.GetComponent<Human>() != null)
             {
+                if (lightSource != null)
+                {
+                    lightSource.GetComponent<Light>().cookie = defaultCookie;
+                    lightSource.GetComponent<Light>().intensity = defaultIntensity;
+                }
+
                 GameObject handObj;
                 if (HandSide == HandSide.Left)
                     handObj = User.transform.Find("LeftHandPoint").gameObject;
@@ -100,6 +123,13 @@ namespace ZeroChance2D
             lightSourcePos.z = VerticalOffset;
         }
 
+        
+        void Update()
+        {
+            Visualization();
+            UpdateLight();
+        }
+
         [Client]
         void FixedUpdate()
         {
@@ -113,7 +143,7 @@ namespace ZeroChance2D
                         Quaternion.Euler(0, 0, lightSourceZRotation),
                         LerpRate * Time.deltaTime);
                 }
-                else
+                else if(User.GetComponent<Human>() != null)
                 {
                     if (HandSide == HandSide.Left)
                     {
@@ -129,6 +159,13 @@ namespace ZeroChance2D
                         lightSource.transform.position = pos;
                         lightSource.transform.rotation = User.transform.Find("RightHandPoint").rotation;
                     }
+                }
+                else if (User.GetComponent<Storage>() != null)
+                {
+                    var pos = User.transform.position;
+                    pos.z = VerticalOffset;
+                    lightSource.transform.position = pos;
+                    lightSource.transform.rotation = User.transform.rotation;
                 }
             }
 
