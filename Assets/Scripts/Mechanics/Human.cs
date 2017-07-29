f69da7bc3cdc4dd3952d2bf1972a2775a7e14633
Dmitry <dimamon6032403@gmail.com>
2017-07-29 22:30:13 +0300

    Equipment advanced

--- Assets/Scripts/Mechanics/Human.cs
+++ Assets/Scripts/Mechanics/Human.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-namespace ZeroChance2D
+namespace ZeroChance2D.Assets.Scripts.Mechanics
 {
     [Serializable]
     public struct Damage
@@ -54,44 +52,72 @@ namespace ZeroChance2D
     [Serializable]
     public enum State
     {
-        Standing, Lying
+        Standing,
+        Lying
     }
 
     [Serializable]
-    public struct Equipment
+    public class Equipment : ICloneable
     {
-        public enum EquipmentSlot { LeftHand, RightHand, Backpack}
+        public enum EquipmentSlot
+        {
+            LeftHand,
+            RightHand,
+            Backpack
+        }
 
         public const int AmountOfSlots = 3;
 
-        public GameObject[] Inventory;
+        [SerializeField] public GameObject[] inventory = new GameObject[AmountOfSlots];
 
         public GameObject this[EquipmentSlot slot]
         {
-            get { return Inventory[(int) slot]; }
-            set { Inventory[(int) slot] = value; }
+            get { return inventory[(int) slot]; }
+            set { inventory[(int) slot] = value; }
         }
 
         public GameObject this[int slot]
         {
-            get { return Inventory[slot]; }
-            set { Inventory[slot] = value; }
+            get { return inventory[slot]; }
+            set { inventory[slot] = value; }
         }
 
         public bool IsEqual(Equipment equipment)
         {
-            for (EquipmentSlot i = 0; i < (EquipmentSlot)AmountOfSlots; i++)
+            for (EquipmentSlot i = 0; i < (EquipmentSlot) AmountOfSlots; i++)
             {
-                if(this[i] != equipment[i])
+                if (this[i] != equipment[i])
                     return false;
             }
 
             return true;
         }
 
-        public Equipment(int amount)
+        public int IndexOf(GameObject element)
         {
-            Inventory = new GameObject[amount];
+            for (int i = 0; i < AmountOfSlots; i++)
+            {
+                if (inventory[i] == element)
+                    return i;
+            }
+
+            return -1;
+        }
+
+
+        public Equipment()
+        {
+            inventory = new GameObject[AmountOfSlots];
+        }
+
+        public object Clone()
+        {
+            Equipment clone = new Equipment();
+            for (int i = 0; i < AmountOfSlots; i++)
+            {
+                clone[i] = inventory[i];
+            }
+            return clone;
         }
     }
 
@@ -105,6 +131,6 @@ namespace ZeroChance2D
         public float Nutrition = 100f;
         public State State;
         public float RotationSpeed = 60f;
-        public Equipment Equipment = new Equipment(Equipment.AmountOfSlots);
+        public Equipment Equipment = new Equipment();
     }
 }
