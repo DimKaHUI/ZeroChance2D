53261692558e5266071a789c847bddeb58aaa956
Dmitry <dimamon6032403@gmail.com>
2017-07-30 14:04:09 +0300

    Created backpack

---
+++ Assets/Scripts/Items/Backpack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZeroChance2D.Assets.Scripts.Mechanics;
+
+namespace ZeroChance2D.Assets.Scripts.Items
+{
+
+    [RequireComponent(typeof(Storage))]
+    public class Backpack : Item
+    {
+        private bool showsUi = false;
+        private Storage storage;
+
+        // Use this for initialization
+        void Start()
+        {
+            BackHoldable = true;
+            storage = gameObject.GetComponent<Storage>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Visualization();
+        }
+
+        public override void Use()
+        {
+            storage.ShowGui(User);
+        }
+    }
+}
