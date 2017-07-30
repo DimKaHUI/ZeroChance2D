627337ac04233a5fade712a42883e7fdf6bce780
Dmitry <dimamon6032403@gmail.com>
2017-07-30 14:07:07 +0300

    Created BExp script

---
+++ Assets/Scripts/UI/BackpackExplorer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ZeroChance2D.Assets.Scripts.UI
+{
+
+    public class BackpackExplorer : MonoBehaviour
+    {
+        public GameObject ItemLinePrefab;
+        public GameObject ItemButtonPrefab;
+
+    }
+}
